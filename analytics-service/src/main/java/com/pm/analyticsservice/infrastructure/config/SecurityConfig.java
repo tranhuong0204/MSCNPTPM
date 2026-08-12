96e2767c7a2d9dc29017ee62b7adf78387dd287f
tranhuong0204 <huongtran4002@gmail.com>
2026-08-12 22:39:59 +0700

    test

--- analytics-service/src/main/java/com/pm/analyticsservice/infrastructure/config/SecurityConfig.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/infrastructure/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/internal/**").permitAll() // cho phép các service gọi nhau internal
                         .requestMatchers("/actuator/**").permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .build();
