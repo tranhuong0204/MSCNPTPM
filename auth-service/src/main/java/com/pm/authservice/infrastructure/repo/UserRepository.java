e3530264a79ade648c807677109075c93ef26477
tranhuong0204 <huongtran4002@gmail.com>
2026-08-16 14:29:30 +0700

    feat: add face login feature

--- auth-service/src/main/java/com/pm/authservice/infrastructure/repo/UserRepository.java
+++ auth-service/src/main/java/com/pm/authservice/infrastructure/repo/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    Optional<User> findByID(UUID id);
+    Optional<User> findById(UUID id);
 }
